1d331bd37d7a2529edff1080886c1bcfb106fe0b
s0kil <dtechtours@gmail.com>
2020-09-14 18:57:50 -0400

    Partially Complete Comments/Article Relationship

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,22 @@
 class ArticlesController < ApplicationController
   include ArticlesHelper
-  
+
   def index
     @articles = Article.all
   end
 
   def show
     article_id = params[:id]
-    @article = Article.find(article_id)
+    @article   = Article.find(article_id)
+
+    @comment            = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
     @article = Article.new
   end
-  
+
   def create
     # @article = Article.new(params[:article])
     # if @article.save
@@ -37,21 +40,21 @@ class ArticlesController < ApplicationController
 
     flash.notice = "Article '#{@article.title}' Deleted!"
     redirect_to articles_url
-    
+
   end
-  
+
   def edit
     @article = Article.find(params[:id])
   end
-  
+
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
 
     flash.notice = "Article '#{@article.title}' Updated!"
-  
+
     redirect_to article_path(@article)
   end
-  
+
 
 end
